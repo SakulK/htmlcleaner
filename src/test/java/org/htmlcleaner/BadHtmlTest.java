bc6520798ae171e2b0a4a22e72154362aa883b5b
SakulK <sakulk@gmail.com>
2012-11-09 18:00:15 +0100

    little cleanup in pom and added a test case with bad html that causes OutOfMemoryException (commented out until its fixed)

---
+++ src/test/java/org/htmlcleaner/BadHtmlTest.java
@@ -0,0 +1,22 @@
+package org.htmlcleaner;
+
+import junit.framework.TestCase;
+import java.io.File;
+import java.io.IOException;
+
+
+public class BadHtmlTest extends TestCase {
+
+    private HtmlCleaner cleaner;
+    private CleanerProperties props;
+
+    protected void setUp() throws Exception {
+        cleaner = new HtmlCleaner();
+        props = cleaner.getProperties();
+    }
+
+    public void testFieldsetNotInForm_shouldNotDieOutOfMemory() throws IOException {
+        //TagNode node = cleaner.clean(new File("src/test/resources/test10.html"));
+        //assertNotNull(node);
+    }
+}
